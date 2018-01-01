4aebd86ada4a2c1a6f5bcea18d3649f9a35b1e5f
Mike <mquon@outlook.com>
2018-01-01 12:42:01 -0800

    update code

--- page_16_answer.js
+++ page_16_answer.js
@@ -1,7 +1,6 @@
 // Page 16 Question 1 - Setting and Swapping
 
 function swapping(){
-
     var myNumber = 42;
     var myName = 'Mike';
     var temp = myNumber;
@@ -15,7 +14,6 @@ swapping();
 //Page 16 Question 2 - Print -52 to 1066
 
 function printIntegers(){
-
     var sum = 0;
         for(var i = -52; i <= 1066; i++){
             sum=i;
@@ -27,7 +25,6 @@ printIntegers()
 //Page 16 Question 3 - Don't Worry, Be Happy
 
 function beCheerful(){
-
     var gm = 0;
     for(var i = 1; i<=98; i++){
         gm = i;
@@ -52,7 +49,6 @@ multiples();
 //Page 16 Question 5 - Printing Integers with While
 
 function intWhile(){
-
     var total = 1999;
     while(total<5280){
         total++;
@@ -64,7 +60,6 @@ intWhile();
 //Page 16 - Question 6 - You Say It’s Your Birthday
 
 function birthDay(month,day){
-
     var birthDay ="";
     if((month == 11 || month == 21) && (day == 21 || day == 11)){
         console.log("How did you know?");
@@ -80,6 +75,7 @@ function leapYear(year){
     
     if(year % 4 ==0 && (year % 100 != 0) || (year % 400 == 0)){
         console.log("It is a Leap Year")
+
         }else{
             console.log("it is not a Leap Year");
         }
@@ -89,13 +85,13 @@ leapYear(2016);
 //Page 16 - Question 8 - Print and Count
 
 function printCount(){
-
     var count = 0;
     for(var i = 512; i <= 4096; i++){
-    if(i % 5 == 0){
-        count++;
-        console.log('sum', i);
-        }    
+        if(i % 5 == 0){
+            count++;
+                console.log('sum', i);
+        }
+        
     }
     console.log('total count', count);
 }
@@ -106,10 +102,11 @@ printCount();
 function multiplesOfSix(){
     var x = 0;
     while(x <= 60000){
-        if (x % 6 == 0){    
+        if (x % 6 == 0){
+            
                 console.log(x);
         }
-            x++;
+        x++;
     }
 }
 multiplesOfSix();
@@ -118,12 +115,12 @@ multiplesOfSix();
 
 function dojoWay(){
         for(var i = 0; i <=100; i++){
-        if(i % 5 == 0){
+            if(i % 5 == 0){
                 console.log("Coding");
-        }if(i % 10 == 0){
+            }if(i % 10 == 0){
                 console.log("Dojo");
+            }
         }
-    }
 }
 dojoWay();
 
@@ -138,11 +135,10 @@ whatDo("incoming");
 //Page 16 - Question 12 - Whoa, That Sucker's Huge...
 
 function whoa(){
-
     var sum = 0;
     for(var i = -300000; i <= 300000; i++){
-    if(i % 2 !== 0){
-        sum+=i;
+        if(i % 2 !== 0){
+            sum+=i;
         }
     }   
     console.log(sum);
@@ -152,7 +148,6 @@ whoa();
 //Page 16 - Question 13 - Countdown by Fours
 
 function countDown(){
-
     var count = 2016;
     while(count > 0){
         console.log(count);
@@ -164,21 +159,19 @@ countDown();
 //Page 16 - Question 14 - Flexible Countdown
 
 function flexibleCount(lowNum, highNum, mult){
-
-    for(var i=highNum; i > lowNum; i-=mult){
-        console.log(i);
-    }
+ for(var i=highNum; i > lowNum; i-=mult){
+     console.log(i);
+ }
 }
-flexibleCount();
+ flexibleCount();
 
  //Page 16 - Question 15 - The Final Countdown
 
  function finalCount(param1, param2, param3, param4){
-
     while(param2 < param3){
         if(param2 % param4 !=0){
-        if(param2 % param1 ===0){
-            console.log(param2);
+            if(param2 % param1 ===0){
+                console.log(param2);
             }
         }
         param2++;
